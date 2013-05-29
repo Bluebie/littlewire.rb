166e26b7cb1b3d14b18da0ebd7637010cd5bc429
Bluebie <a@creativepony.com>
2013-05-29 10:00:00 +1000

    ws2811 colors example - make it simpler with new api bits

--- examples/ws2811/colors.rb
+++ examples/ws2811/colors.rb
@@ -6,15 +6,14 @@ num_leds = 64
 
 loop do
   puts "red"
-  wire.ws2811.colors = ['red'] * num_leds
-  wire.ws2811.output pin
+  wire.ws2811(pin).send ['red'] * num_leds
   sleep speed
+  
   puts "green"
-  wire.ws2811.colors = ['green'] * num_leds
-  wire.ws2811.output pin
+  wire.ws2811(pin).send ['green'] * num_leds
   sleep speed
+  
   puts "blue"
-  wire.ws2811.colors = ['blue'] * num_leds
-  wire.ws2811.output pin
+  wire.ws2811(pin).send ['blue'] * num_leds
   sleep speed
 end
